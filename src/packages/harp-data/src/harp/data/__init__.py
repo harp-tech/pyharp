f9034a5695eb8188347d134cf00afb47a6516502
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-07-26 09:38:16 -0700

    Add syntactic sugar for dataset creation

--- src/packages/harp-data/src/harp/data/__init__.py
+++ src/packages/harp-data/src/harp/data/__init__.py
@@ -1,4 +1,4 @@
-from ._dataset import DatasetReader, default_file_resolver
+from ._dataset import DatasetReader, create_dataset_reader, default_file_resolver
 from ._reader import REFERENCE_EPOCH, parse_to_dataframe, payload_to_dataframe
 from ._write import to_buffer, to_file
 
@@ -8,6 +8,7 @@ __all__ = [
     "to_buffer",
     "to_file",
     "DatasetReader",
+    "create_dataset_reader",
     "default_file_resolver",
     "REFERENCE_EPOCH",
 ]
